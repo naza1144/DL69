27718d21ee62ac321979e96a52ede3efb62ed044
naza1144-dev-linux <knnbos77@gmail.com>
2026-09-09 10:07:33 +0700

    chore: rename project from wk09 to wk_10 throughout configuration files

--- wk_10/wsgi.py
+++ wk_10/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wk09.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wk_10.settings')
 
 application = get_wsgi_application()
